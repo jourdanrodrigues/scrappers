5d1c291074eab50e929c67a2b1995bee2c23941f
Thiago Jourdan <thiagojourdan@gmail.com>
2023-02-15 00:23:49 -0300

    :sparkles: Add a handler for Vercel

---
+++ api/index.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler
+
+from main import SyncEntries
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        SyncEntries.sync()
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
