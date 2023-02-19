deaf429392c8b996f483b1cf1697f5572783c833
Thiago Jourdan <thiagojourdan@gmail.com>
2023-02-19 16:45:02 -0300

    :memo: Reformat API code and remove unused "registries" endpoint

--- src/pages/api/alerts.ts
+++ src/pages/api/alerts.ts
@@ -1,26 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type {NextApiRequest, NextApiResponse} from 'next'
-import {Prisma} from '@/clients/prisma'
-import {Registries} from '@/registries'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Prisma } from '@/clients/prisma';
+import { Registries } from '@/registries';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Record<string, any>>,
+  res: NextApiResponse<Record<string, any>>
 ) {
   const requisitions = await Prisma.requisition.findMany({
-    include: {listeners: true, phases: true},
-  })
+    include: { listeners: true, phases: true },
+  });
   const promises = requisitions.map(async (requisition) => {
-    const client = Registries.getClientById(requisition.registryId)
-    if (!client) return []
-    const phases = await client.fetchPhases(requisition)
-    if (requisition.phases.length === phases.length) return []
-    const newPhases = phases.slice(requisition.phases.length)
+    const client = Registries.getClientById(requisition.registryId);
+    if (!client) return [];
+    const phases = await client.fetchPhases(requisition);
+    if (requisition.phases.length === phases.length) return [];
+    const newPhases = phases.slice(requisition.phases.length);
     await Prisma.phase.createMany({
-      data: newPhases.map((phase) => ({...phase, requisitionId: requisition.id})),
-    })
-    return newPhases
-  })
-  const outputs = await Promise.all(promises)
-  res.status(200).json(outputs.flat())
+      data: newPhases.map((phase) => ({
+        ...phase,
+        requisitionId: requisition.id,
+      })),
+    });
+    return newPhases;
+  });
+  const outputs = await Promise.all(promises);
+  res.status(200).json(outputs.flat());
 }
