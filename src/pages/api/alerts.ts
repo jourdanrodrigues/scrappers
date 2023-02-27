ba5461626b4a6fc3e2dab245cd903de72c0339a2
Thiago Jourdan <thiagojourdan@gmail.com>
2023-02-26 23:27:15 -0300

    :ambulance: Remove "POST" limitation on alerts endpoint

--- src/pages/api/alerts.ts
+++ src/pages/api/alerts.ts
@@ -16,9 +16,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Record<'message', string>>
 ) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
   const transport = nodemailer.createTransport({
     service: 'gmail',
     auth: { user: sourceEmail, pass: emailPassword },
